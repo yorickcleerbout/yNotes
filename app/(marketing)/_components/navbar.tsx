8b11c1e2a69137ddc48b70625b4fa615a4f8d1aa
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-10-27 17:08:24 +0200

    Added Authentication to Navbar Component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,17 +1,46 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
 
     return (
         <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">Login</Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">Get yNotes Free</Button>
+                        </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter yNotes
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/"/>
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
