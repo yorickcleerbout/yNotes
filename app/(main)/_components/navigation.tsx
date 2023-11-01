86eaf671ecfabd5704faa0f3681b1e13925d6184
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-11-01 20:19:10 +0100

    Added DocumentList component to Sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,21 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react"
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react"
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
-    const isMobile = useMediaQuery("(max-width: 768px)")
+    const isMobile = useMediaQuery("(max-width: 768px)");
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -89,6 +95,16 @@ export const Navigation = () => {
 
     };
 
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note."
+        });
+    };
+
     return (
         <>
             <aside
@@ -111,10 +127,28 @@ export const Navigation = () => {
                 
                 <div>
                     <UserItem />
+                    <Item
+                        label="Search"
+                        icon={Search}
+                        isSearch
+                        onClick={() => {}}
+                    />
+                    <Item
+                        label="Settings"
+                        icon={Settings}
+                        onClick={() => {}}
+                    />
+                    <Item 
+                        onClick={handleCreate} 
+                        label="New page" 
+                        icon={PlusCircle} 
+                    />
                 </div>
+
                 <div className="mt-4">
-                    <p>Documents</p>
+                    <DocumentList />
                 </div>
+
                 <div
                     onMouseDown={handleMouseDown}
                     onClick={resetWidth}
