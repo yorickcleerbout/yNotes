44c5ba0fdfa17c0f51e91e58a9e908dd7426e137
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-11-01 16:23:13 +0100

    Added Sidebar boilerplate

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
     children
@@ -24,8 +25,11 @@ const MainLayout = ({
     }
 
     return ( 
-        <div>
-            {children}
+        <div className="h-full flex dark:bg-[#1F1F1F]">
+            <Navigation />
+            <main className="flex-1 h-full overflow-y-auto">
+                {children}
+            </main>
         </div>
      );
 }
