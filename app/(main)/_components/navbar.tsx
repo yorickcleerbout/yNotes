e1e2bc0aab19c87e6e9651084562625c6e47d368
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-11-05 12:58:58 +0100

    Added Menu Component for removal of page

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
 import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -25,8 +26,11 @@ export const Navbar = ({
 
     if (document === undefined) {
         return (
-            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         );
     }
@@ -41,6 +45,9 @@ export const Navbar = ({
                 )}
                 <div className="flex items-center justify-between w-full">
                     <Title initialData={document}/>
+                    <div className="flex items-center gap-x-2">
+                        <Menu documentId={document._id} />
+                    </div>
                 </div>
             </nav>
             {document.isArchived && (
