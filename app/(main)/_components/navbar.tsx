0e32a1fb869c027f9a5cbe4ef05cc7ff863a6a4f
Yorick Cleerbout <yorick.cleerbout@i8c.be>
2023-11-05 12:45:09 +0100

    Added Banner Components for Notes in the Trash.

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -42,6 +43,9 @@ export const Navbar = ({
                     <Title initialData={document}/>
                 </div>
             </nav>
+            {document.isArchived && (
+                <Banner documentId={document._id} />
+            )}
         </>
     )
 }
